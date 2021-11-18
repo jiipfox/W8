b58208818594e58c05559f85d9665fcbd80cfb73
Jani Repo <jani.repo@student.lut.fi>
2021-11-18 21:37:54 +0200

    9.3

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -3,9 +3,9 @@ function MyList(props) {
     const items = props.items;
     const header = props.hearer;
     console.log(items);
-     const lis  = items.map((item) => 
-        <li>{item.id} = {item.text}</li>
-            );
+     const lis  = items.map((item) =>
+        //<li>{item.id} = {item.text}</li>
+        <li>{item.text}</li>);
     return  (<ul>{lis}</ul>);
   }
 
