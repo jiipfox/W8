14af81b130f9249bc5309d6e01070f8b9c4adac5
Jani Repo <jani.repo@student.lut.fi>
2021-11-19 08:36:03 +0200

    9.3

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,16 +1,35 @@
 import MyList from "./MyList.js";
+import React, { useState } from 'react';
 
 function MyContainer(){
-    const header = "Not so interesting list component";
     const texts = ["Item 1", "Item 2", "Item 3"];
-    const items = [
+
+    const [count, setCount] = useState(texts.length)
+
+    const [items, setItems] = useState([
         {id: "1", text: texts[0]},
         {id: "2", text: texts[1]},
-        {id: "3", text: texts[2]}];
+        {id: "3", text: texts[2]}]
+        );
+
+    function incrementCount(){
+        setCount(prevCount => prevCount + 1)
+    }
+
+    console.log(count);
+    console.log(items);
+
+
+    // current state ja funktio joka muuttaa
+    //const [currentState, functionThatUpdatesTheState] = useState(value);
+    const header = "Not so interesting list component";
     
     return (
         <>
         <MyList header={header} items={items} />
+        <textarea id="textarea"></textarea>
+        <p>{count}</p>
+        <button onClick={incrementCount}>Append item</button>
       </>
     )
 }
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -5,7 +5,7 @@ function MyList(props) {
     console.log(items);
      const lis  = items.map((item) =>
         //<li>{item.id} = {item.text}</li>
-        <li key ={item.id.toString()}>{item.text}</li>);
+        <li key ={item.id}>{item.text}</li>);
     return  (
       <div>
       <h1>{header}</h1>
